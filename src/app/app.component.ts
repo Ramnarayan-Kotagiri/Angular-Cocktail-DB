fb73c54d0d717876f6ba4500ffb916105674530d
Ramnarayan-Kotagiri <kotagiriramnarayan@gmail.com>
2021-07-03 22:49:16 +0530

    Implemented Filters & Search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
 import { Component } from '@angular/core';
+import { CocktailService } from './cocktail.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent {
+
   title = 'angularCocktailDb';
+  hiddenStatus = 'Hide';
+  categories: any = [];
+  glasses: any = [];
+  ingredients: any = [];
+  alcoholicType: any = [];
+  filters = [
+    { name: 'c', value: 'strCategory' },
+    { name: 'i', value: 'strIngredient1' },
+    { name: 'g', value: 'strGlass' },
+    { name: 'a', value: 'strAlcoholic' },
+  ];
+  appliedFilterItems: any = [];
+  activeFilter:string = '';
+  activeSubFilter:string = '';
+  query = '';
+  searchEnabled = false;
+  results:any = [];
+  filtersEnabled = false;
+
+  constructor(private cocktail: CocktailService) {}
+
+  chooseFilter(event: any) {
+    this.activeFilter = event.target.value;
+    this.query = ''
+    if (this.activeFilter === 's' || this.activeFilter === 'f') {
+      this.searchEnabled = true;
+      this.filtersEnabled = false;
+    } else {
+      this.searchEnabled = false;
+      let objectKey = this.filters
+        .filter((x) => x.name === event.target.value)
+        .map((x) => x.value);
+      this.cocktail.getFilterItems(event.target.value, objectKey[0]).subscribe(
+        (resp: any) => {
+          if (resp) {
+            this.appliedFilterItems = resp;
+          }
+        },
+        (error: any) => {
+          alert('ERROR: There was an error fetching the sub filters');
+        }
+      );
+    }
+  }
+
+  chooseSubFilter(event: any){
+    this.activeSubFilter = event.target.value;
+    this.cocktail.getFilteredCocktails(this.activeFilter,this.activeSubFilter).subscribe(
+      (resp) => {
+        this.results = resp;
+        this.filtersEnabled = true;
+      },
+      (error: any) => {
+        alert('ERROR: There was an error fetching the filtered cocktails result.');
+      })
+  }
+
+  searchBy(){
+  this.cocktail.searchForCocktailsMatchingQuery(this.activeFilter,this.query).subscribe(
+    (resp)=> {
+      this.results = resp;
+      this.filtersEnabled = true;
+    },
+    (error: any) => {
+      alert('ERROR: There was an error fetching the filtered cocktails result.');
+    }
+  )
+  }
 }
---
+++ src/app/cocktail.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CocktailService {
+  readonly API_PROVIDER_URL = 'https://www.thecocktaildb.com/api/json/v1/1/';
+
+  constructor(private http: HttpClient) {}
+
+  getFilterItems(filter: string,key: string){
+    const url = this.API_PROVIDER_URL + 'list.php?'+filter+'=list';
+    return this.http.get<any>(url).pipe(
+      map(x => x.drinks.map((x: any) => x[key])),
+      catchError((err) => {
+        console.log('error caught in service');
+        console.error(err);
+        return throwError(err);
+      })
+    );
+  }
+
+  getFilteredCocktails(filter: string,subFilter: string){
+    const url = this.API_PROVIDER_URL + 'filter.php?'+filter+'='+subFilter;
+    return this.http.get<any>(url).pipe(
+      map(x => x.drinks),
+      catchError((err) => {
+        console.log('error caught in service');
+        console.error(err);
+        return throwError(err);
+      })
+    );
+  }
+
+  searchForCocktailsMatchingQuery(filter: string,query: string){
+    const url = this.API_PROVIDER_URL + 'search.php?'+filter+'='+query;
+    return this.http.get<any>(url).pipe(
+      map(x => x.drinks),
+      catchError((err) => {
+        console.log('error caught in service');
+        console.error(err);
+        return throwError(err);
+      })
+    );
+  }
+}
